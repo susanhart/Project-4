38f61799a32b27bf7e86c20a52d484d8515fe71e
Sue <susanhartesquire@yahoo.com>
2019-05-31 12:05:47 -0400

    Commit Step 2 and 3 of project

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = ["car wash", "retail therapy"]
+        this.activePhrase = "you win";
+} 
+}  
